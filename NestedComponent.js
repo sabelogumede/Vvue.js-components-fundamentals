482d79c360c16f2874248aa391f34193fc107e3e
sabelogumede <iamsabelogumede@gmail.com>
2019-04-28 16:06:01 +0200

    nested components

---
+++ NestedComponent.js
@@ -0,0 +1,24 @@
+Vue.component('plan-picker', {
+    template: '#plan-picker-template',
+    data() {
+        return {
+            plans: ['The Hacker','The Single', 'The Curious', 'The Addict']
+        }
+    }
+})
+
+Vue.component('plan', {
+    template: '#plan-template',
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    }
+})
+
+new Vue({
+    el: '#app'
+})
+
+  
